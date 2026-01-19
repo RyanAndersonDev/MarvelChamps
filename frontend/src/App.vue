bc0ab676b93cdd9a94442e96d864ce1a2a9d1234
Ryan Anderson <ryan.anderson.3939@gmail.com>
2026-01-19 01:24:33 -0600

    Adding components to main for rendering and testing

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,46 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import PlayerHand from "./components/PlayerHand.vue";
+import PlayerId from "./components/PlayerId.vue";
+import PlayerDeck from "./components/PlayerDeck.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main>
+    <h1>Marvel Champions</h1>
+    <div class="player-comp-container">
+      <PlayerDeck class="player-deck"/>
+      <PlayerId class="player-id"/>
+      <PlayerHand class="player-hand"/>
+    </div>
+  </main>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+  main {
+    padding: 24px;
+  }
+
+  .player-comp-container {
+    position: fixed;
+    bottom: 20px;
+    left: 20px;
+    z-index: 1000;
+
+    display: flex;
+    align-items: flex-end;
+    gap: 16px;
+  }
+
+
+  .player-deck {
+
+  }
+
+  .player-id {
+
+  }
+
+  .player-hand {
+
+  }
 </style>
