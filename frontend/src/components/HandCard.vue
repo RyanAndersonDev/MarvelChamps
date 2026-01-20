306381812206f9e798bf85c3319b5ee51fd20284
Ryan Anderson <ryan.anderson.3939@gmail.com>
2026-01-20 03:03:40 -0600

    Rough beginning of handcard filling playerhand component without styling

---
+++ frontend/src/components/HandCard.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+    import type { Ally, Event, Upgrade, Support } from '../types/card';
+    defineProps<{ card: Ally | Event | Upgrade | Support }>();
+</script>
+
+<template>
+    <div class="card">
+    <img :src="card.imgPath" />
+    <p>{{ card.name }}</p>
+    <p v-if="'health' in card">Health: {{ card.health }}</p>
+    <p v-if="'counters' in card">Counters: {{ card.counters }}</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
