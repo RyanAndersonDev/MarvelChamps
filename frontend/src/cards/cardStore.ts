e50a16496615deacb6ea9a049009308fbc515336
Ryan Anderson <ryan.anderson.3939@gmail.com>
2026-01-20 03:18:09 -0600

    Updating cardstore to match new types

--- frontend/src/cards/cardStore.ts
+++ frontend/src/cards/cardStore.ts
@@ -1,6 +1,6 @@
 import type { PlayerCardInstance } from "../types/card";
 
 export const cardMap: Map<number, PlayerCardInstance> = new Map<number, PlayerCardInstance>([
-    [1, { name: "Black Cat", side: "player", type: "ally", cost: 2, aspect: "hero", imgPath: "/cards/heroes/BlackCat-Ally.png", health: 2, tags: ["hero for hire"] }],
-    [2, { name: "Backflip", side: "player", type: "event", cost: 0, aspect: "hero", imgPath: "/cards/heroes/Backflip-Event.png", tags: ["defense", "skill"] }]
+    [1, { name: "Black Cat", side: "player", type: "ally", cost: 2, aspect: "hero", imgPath: "/cards/heroes/BlackCat-Ally.png", tags: ["hero for hire"], resources: ["energy"], thw: 1, atk: 1, health: 2 }],
+    [2, { name: "Backflip", side: "player", type: "event", cost: 0, aspect: "hero", imgPath: "/cards/heroes/Backflip-Event.png", tags: ["defense", "skill"], resources: ["physical"] }]
 ])
