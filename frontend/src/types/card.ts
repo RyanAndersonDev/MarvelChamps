c9c3ad2da60a9b3c1e68f409ae6ca4b36251529b
Ryan Anderson <ryan.anderson.3939@gmail.com>
2026-01-20 03:04:26 -0600

    Improving card types

--- frontend/src/types/card.ts
+++ frontend/src/types/card.ts
@@ -9,30 +9,55 @@ export type VillainCardType = "minion" | "treachery" | "side-scheme" | "attachme
 export type Aspect = "neutral" | "hero" | "aggression" | "justice" | "protection" | "leadership";
 
 export interface CardBase {
-    id: string;
-    code: string;
     name: string;
     side: "player" | "villain";
+    imgPath: string;
 }
 
 export interface IdentityCard extends CardBase {
-  id: string;
-  name: string;
-  identityStatus: IdentityStatus;
-  status: CardStatus;
-  hitPoints: number;
-  healing: number;
-  atk: number;
-  def: number;
-  handSizeHero: number;
-  handsizeAe: number;
+    name: string;
+    identityStatus: IdentityStatus;
+    status: CardStatus;
+    hitPoints: number;
+    healing: number;
+    atk: number;
+    def: number;
+    handSizeHero: number;
+    handsizeAe: number;
 }
 
 export interface PlayerCard extends CardBase {
+    name: string;
     side: "player";
     type: PlayerCardType;
     cost: number;
     aspect: Aspect;
+    imgPath: string;
+}
+
+export interface PlayerCardInstance extends PlayerCard {
+    id?: number;
+    health?: number;
+    counters?: number;
+    tags?: string[];
+}
+
+export interface Ally extends PlayerCardInstance {
+    exhausted: boolean;
+    health: number;
+}
+
+export interface Event extends PlayerCardInstance {
+    tags: string[];
+}
+
+export interface Upgrade extends PlayerCardInstance {
+    exhausted: boolean;
+    counters: number;
+}
+
+export interface Support extends PlayerCardInstance {
+    exhausted: boolean;
 }
 
 export interface VillainCard extends CardBase {
@@ -42,3 +67,5 @@ export interface VillainCard extends CardBase {
 }
 
 export type Card = IdentityCard | PlayerCard | VillainCard;
+
+export type Resource = "physical" | "mental" | "energy" | "wild";
