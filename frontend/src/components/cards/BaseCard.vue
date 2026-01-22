0b82fa8c2241f2a4687af7e9aeb7825176b6fd73
Ryan Anderson <ryan.anderson.3939@gmail.com>
2026-01-22 03:09:57 -0600

    Adding base card class for styling consistency

---
+++ frontend/src/components/cards/BaseCard.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+    defineProps<{
+        imgPath: string;
+        alt?: string;
+    }>();
+</script>
+
+<template>
+  <div class="base-card">
+    <img :src="imgPath" :alt="alt ?? 'Card'" class="hover-zoom"/>
+  </div>
+</template>
+
+<style scoped>
+    .base-card {
+        width: 200px;
+        height: 300px;
+        border-radius: 12px;
+        box-shadow: 0 4px 12px rgba(0,0,0,0.3);
+        /* overflow: hidden; */
+    }
+
+    .base-card img {
+        width: 100%;
+        height: 100%;
+        /* object-fit: cover; */
+    }
+
+    .hover-zoom {
+        transition: transform 0.3s ease, box-shadow 0.3s ease;
+    }
+
+    .hover-zoom:hover {
+        transform: scale(1.5);
+        transform-origin: bottom;
+        box-shadow: 0 10px 20px rgba(0, 0, 0, 0.3);
+        z-index: 1000;
+        position: relative;
+    }
+</style>
