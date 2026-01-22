ff43e247d67f5d4792ffedde7ced237af3e12b85
Ryan Anderson <ryan.anderson.3939@gmail.com>
2026-01-22 03:10:18 -0600

    Adding player tableau components

---
+++ frontend/src/components/cards/PlayerTableauCard.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+  import BaseCard from './BaseCard.vue';
+  import type { Ally, Upgrade, Support } from '../../types/card';
+  import type { PlayerCardType } from '../../types/card';
+    
+  const props = defineProps<{ card: Ally | Upgrade | Support }>();
+
+  function doAction() : void {
+    const cardType : PlayerCardType = props.card.type;
+    console.log("test");
+  }
+</script>
+
+<template>
+  <div class="tableau-card-wrapper">
+    <BaseCard :img-path="card.imgPath" />
+
+    <div class="button-row">
+      <button @click="doAction">Action</button>
+      <button v-if="props.card.type === 'ally'" @click="doAction">Attack</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .tableau-card-wrapper {
+    display: flex;            /* make this a flex container */
+    flex-direction: column;   /* stack children vertically: card first, buttons below */
+    align-items: center;      /* horizontally center all children */
+    gap: 8px;                 /* space between card and buttons */
+  }
+</style>
---
+++ frontend/src/components/player-board/PlayerTableau.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+    import { ref } from 'vue';
+    import type { Ally, Event, Upgrade, Support } from '../../types/card';
+    import PlayerTableauCard from '../cards/PlayerTableauCard.vue';
+    // TODO: Should I translate HandCard => TableauCard, or
+    //  destroy and recreate with createTableauCard function
+    import { createTableauCard } from '../../cards/cardFactory';
+
+    const tableau = ref<(Ally | Upgrade | Support)[]>([]);
+
+    tableau.value.push(createTableauCard(1, 1));
+</script>
+
+<template>
+    <div class="tableau-container-wrapper">
+        <div class="tableau-container">
+            <PlayerTableauCard
+                v-for="card in tableau"
+                :key="card.id"
+                :card="card"
+            />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    
+</style>
