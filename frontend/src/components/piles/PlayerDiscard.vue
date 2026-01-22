2073c42ee1ce348553d1972f8e3a4d5dc1b484d7
Ryan Anderson <ryan.anderson.3939@gmail.com>
2026-01-22 03:10:48 -0600

    Adding discard pile component

---
+++ frontend/src/components/piles/PlayerDiscard.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+    import { ref } from "vue";
+    import { getCardImgPathById } from '../../cards/cardStore'
+
+    const pile = ref<number[]>([1]);
+
+    function setImgAsLastPlayed() : string {
+        if (pile.value.length === 0) {
+            return "";
+        }
+
+        const lastCardId : number = pile.value[pile.value.length - 1] as number;
+
+        return getCardImgPathById(lastCardId);
+    }
+
+    function peekPile() {
+        // TODO: implement peek
+    }
+</script>
+
+<template>
+    <div class="pile-container">
+        <div class="pile-card-container">
+            <img
+                :src= setImgAsLastPlayed()
+                alt="Discard Pile"
+                class="pile-card"
+            />
+
+            <div class="pile-counter">
+                {{ pile.length }}
+            </div>
+        </div>
+
+        <div class="button-row">
+            <button @click="peekPile">Peek</button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
