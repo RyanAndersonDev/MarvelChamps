1fc2b986ab120413ba4e037f452740138a43d9d5
Ryan Anderson <ryan.anderson.3939@gmail.com>
2026-01-25 03:29:06 -0600

    Making test deck order not follow IDs to avoid future debugging annoyance

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -13,7 +13,7 @@
 
   const villainCard = ref<VillainIdentityCardInstance>(createVillainIdentityCard(1));
 
-  const deckIds = ref<number[]>([ 8, 3, 2, 5, 1, 2, 3, 4, 5, 6, 7, 8]);
+  const deckIds = ref<number[]>([ 8, 7, 6, 5, 4, 3, 2, 1]);
   const hand = ref<(Ally | Event | Upgrade | Support)[]>([]);
   const discardIds = ref<number[]>([]);
   const tableauCards = ref<(Ally | Upgrade | Support)[]>([]);
